d10b013f6da6b96e384d3536fea0a04ea3755e98
Sahil Chaurasiya <sahil.chaurasiya.dev@gmail.com>
2026-01-01 18:38:56 +0530

    Day 1: Python setup and basics

---
+++ day1.py
@@ -0,0 +1,12 @@
+name = input("enter your name:")
+age = int(input("Enter the age:"))
+
+future_age = age + 5
+
+print("Hello", name)
+print("Your age after 5 years will be: ", future_age)
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+print("Sum is: ", num1 + num2)
